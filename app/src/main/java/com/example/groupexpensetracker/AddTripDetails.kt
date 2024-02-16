980ff4d600dc819ce0920b870cfe25e952be0b43
Himanshu <himanshury1231@gmail.com>
2024-02-16 21:35:01 +0530

    Add Trip Details screen UI created.

---
+++ app/src/main/java/com/example/groupexpensetracker/AddTripDetails.kt
@@ -0,0 +1,11 @@
+package com.example.groupexpensetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTripDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_trip_details)
+    }
+}
